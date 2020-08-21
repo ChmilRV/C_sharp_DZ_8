c2fcad659b689f82f97377dce7c00e8b4633fe33
chmilrv@gmail.com <chmilrv@gmail.com>
2020-08-21 11:36:39 +0300

    1136

--- C_sharp_DZ_8_1/Program.cs
+++ C_sharp_DZ_8_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,71 @@ namespace C_sharp_DZ_8_1
     
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
+            Dictionary<string, string> myDict = new Dictionary<string, string>()
+            {
+                ["Австралия"] = "Australia",
+                ["Австрия"] = "Austria",
+                ["Азербайджан"] = "Azerbaijan",
+                ["Албания"] = "Albania",
+                ["Алжир"] = "Algeria",
+                ["Ангола"] = "Angola",
+                ["Андорра"] = "Andorra",
+                ["Антигуа и Барбуда"] = "Antigua and Barbuda",
+                ["Аргентина"] = "Argentina",
+                ["Армения"] = "Armenia",
+                ["Афганистан"] = "Afghanistan",
+            };
+            WriteLine("Содержимое словаря:");
+            foreach (KeyValuePair<string, string> word in myDict)
+            {
+                WriteLine($"{word.Key}\t\t{word.Value}");
+            }
+            WriteLine("Выбор направления перевода\n" +
+                "1 - Рус - Англ\n" +
+                "2 - Англ - Рус");
+            switch (ReadLine())
+            {
+                case "1":
+                    WriteLine("Введите слово для перевода:");
+                    string key;
+                    if (myDict.TryGetValue(ReadLine(), out key))
+                    {
+                        WriteLine(key);
+                    }
+                    else
+                    {
+                        WriteLine("Нет в словаре.");
+                    }
+                    break;
+                case "2":
+                    WriteLine("Введите слово для перевода:");
+                    string _Value = ReadLine();
+                    if (myDict.ContainsValue(_Value))
+                    {
+                        WriteLine(myDict[_Value]);
+                    }
+                    else
+                    {
+                        WriteLine("Нет в словаре.");
+                    }
+                    break;
+
+
+            }
 
 
 
 
+            
+
+
+
+
+
+
+            ReadKey();
         }
     }
 }
